c26a3854ec9ce415455e22d8d1c743ea6d240bfb
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-27 18:44:24 -0500

    updateg

---
+++ java/src/FixLoop.java
@@ -0,0 +1,11 @@
+public class FixLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            String output = (i % 2 == 0) ? "even" : "odd";
+            System.out.println("Number " + i + " is " + output);
+
+            // Decrement i inside the loop
+            i--;
+        }
+    }
+}
