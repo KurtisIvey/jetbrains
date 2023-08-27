635c4cefc39fcd930da962c8fe25b16b3c216817
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-27 18:29:41 -0500

    add divisible by n

---
+++ java/src/NumDivisByN.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NumDivisByN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        int count = 0;
+        for (int i = a; i <= b; i += n) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
