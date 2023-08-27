c889b081797d6c243856f45dfdb55b58e28374c6
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-27 18:10:15 -0500

    update

---
+++ java/src/LargestNum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LargestNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int largest = 0;
+        // follow while with whitespace
+        while (scanner.hasNextInt()) {
+            int scanned = scanner.nextInt();
+            // if must be followed by whitespace
+            if (scanned > largest) {
+                largest = scanned;
+            }
+        }
+        System.out.println(largest);
+    }
+}
