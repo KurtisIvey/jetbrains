bc5244f1cd1fc475dc498ee520e05888b5eb4708
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-29 14:05:46 -0500

    update

--- java/src/AnalyzeWords.java
+++ java/src/AnalyzeWords.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class AnalyzeWords {
     public static void main(String[] args) {
+        // take input
+        System.out.print("Input string: ");
+        // read input
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         for(int i = 0; i <input.length();i++) {
