274560e413556f412034097a44e62c203bcd9674
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-31 09:52:10 -0500

    finally understand chuck encoded to text

--- java/src/ChuckNorrisDecrypt.java
+++ java/src/ChuckNorrisDecrypt.java
@@ -1,5 +1,19 @@
-import java.util.Scanner;
+/*
+p: massive zero string
+r: return conversion into normal text
+ep: create 2 strings for storing empty initialized
+    use for loop that increments by 2. because all the even indexes indicate whether zero or one
+    if 0 (its a 1) add to binary string a 1 that's repeated i + 1 times
+    if 00 (its a 0) add to binary string a 0 that's repeated i + 1 times
 
+    // convert to text
+    for loop to iterate through binary that increments += 7 on i due to 7 bit binary
+    use Integer.parseInt with the argument of binary.substring(i, i+7) with a radix of 2 to convert the char
+    add char to text
+
+    finally, print out the text
+ */
+import java.util.Scanner;
 public class ChuckNorrisDecrypt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -20,13 +34,12 @@ public class ChuckNorrisDecrypt {
                     break;
             }
         }
-
-        // Binary to Text Conversion
         for (int i = 0; i < binary.length(); i += 7) {  // Process binary in chunks of 7 bits
             // Convert the chunk to an integer, then to a character, and add to text
             char c = (char) Integer.parseInt(binary.substring(i, i + 7), 2);
             text += c;
         }
+
 
         System.out.println("The result: ");
         System.out.println(text);  // Print the decrypted text
