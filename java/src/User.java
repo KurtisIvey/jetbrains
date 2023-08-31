066cde950bd9043f6d21ff0ed074daeb66a8eff9
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-31 15:40:08 -0500

    update

---
+++ java/src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    // fields
+    String login;
+    String firstName;
+    String lastName;
+
+    // Constructor
+    User(String login, String firstName, String lastName) {
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
