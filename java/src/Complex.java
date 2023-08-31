56772aa3c0e2e0c1b60eea15c9efc777ecf27cad
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-31 15:15:38 -0500

    update

---
+++ java/src/Complex.java
@@ -0,0 +1,15 @@
+public class Complex {
+    double real;
+    double image;
+
+    public void add(Complex num) {
+        // num is itself an instance of complex class, so it must reference .real & .image
+        this.real += num.real;
+        this.image += num.image;
+    }
+
+    public void subtract(Complex num) {
+        this.real -= num.real;
+        this.image -= num.image;
+    }
+}
