a6b27bfe9216948fa686d3c21505ad89d83b7762
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-01 23:06:30 -0500

    update

--- java/src/September01/DetermineGender.java
+++ java/src/September01/DetermineGender.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class DetermineGender {
     public static String chromosomeCheck(String sperm) {
-        if(Objects.equals(sperm, "XY")) {
+        if(sperm == "XY") {
             return "Congratulations! You're going to have a son.";
         } else {
             return "Congratulations! You're going to have a daughter.";
