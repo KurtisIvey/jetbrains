58953faf4dd81063c55ffcdcce3848ff8ea88cce
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-22 13:51:54 -0500

    update

---
+++ java/src/September22/GalacticFormatting.java
@@ -0,0 +1,11 @@
+package September22;
+
+public class GalacticFormatting {
+    public static void main(String[] args) {
+        String name = "galactic hunter";
+        String learning  = "java";
+        //write your code below
+        System.out.printf("%s learning %s", name, learning);
+
+    }
+}
