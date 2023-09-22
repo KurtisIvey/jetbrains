dad15f38e57448ffb47eeb8054b87f8aad7aa3e9
KurtisIvey <kurtiveycodes@gmail.com>
2023-09-22 13:54:49 -0500

    update

---
+++ java/src/September22/CheckVal.java
@@ -0,0 +1,16 @@
+package September22;
+
+import java.util.Scanner;
+
+public class CheckVal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int input = scanner.nextInt();
+        if (input < 10 && input > 0) {
+            System.out.print("true");
+        } else {
+            System.out.print("false");
+        }
+    }
+}
