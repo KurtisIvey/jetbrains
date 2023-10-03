5fcd444fdc2f929494029308358482d4d46149b7
KurtisIvey <kurtiveycodes@gmail.com>
2023-10-03 16:44:24 -0500

    update

---
+++ java/src/October3/ManualForLoop.java
@@ -0,0 +1,17 @@
+package October3;
+
+import java.util.Scanner;
+
+public class ManualForLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int product = 1;
+        for(int i = start;i<end;i++) {
+            product = product * i;
+        }
+        System.out.println(product);
+    }
+}
