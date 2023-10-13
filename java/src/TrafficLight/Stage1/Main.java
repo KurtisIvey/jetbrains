edf5c614e2216f4f7131d1724d484c875eab2b32
KurtisIvey <kurtiveycodes@gmail.com>
2023-10-13 12:53:18 -0500

    update

---
+++ java/src/TrafficLight/Stage1/Main.java
@@ -0,0 +1,15 @@
+package TrafficLight.Stage1;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("""
+            Welcome to the traffic management system!
+            Menu:
+            1. Add
+            2. Delete
+            3. System
+            0. Quit
+            """);
+
+    }
+}
