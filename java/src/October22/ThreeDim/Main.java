d5a9e615dac77f4bb2c844e1f7df7eb5d4be3e96
KurtisIvey <kurtiveycodes@gmail.com>
2023-10-22 13:41:26 -0500

    update

--- java/src/October22/ThreeDim/Main.java
+++ java/src/October22/ThreeDim/Main.java
@@ -7,18 +7,21 @@ public class Main {
         System.out.print(Arrays.deepToString(createCube()));
 
     }
+
     public static int[][][] createCube() {
         // write your code here
         int[][][] threeDimArray = new int[3][3][3];
-
+        int element = 0;
         // Populate the array with values
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
                     // Calculate the value for each element based on its position
-                    threeDimArray[i][j][k] = i * 3 + j * 3 + k;
+                    threeDimArray[i][j][k] = element;
+                    element++;
                 }
             }
+            element = 0;
         }
         return threeDimArray;
     }
