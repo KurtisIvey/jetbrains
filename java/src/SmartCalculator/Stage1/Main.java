0958116ee69c15a97e80c620d0128ef23542abe1
KurtisIvey <kurtiveycodes@gmail.com>
2023-10-27 16:30:53 -0500

    update

---
+++ java/src/SmartCalculator/Stage1/Main.java
@@ -0,0 +1,14 @@
+package SmartCalculator.Stage1;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.print(num1+num2);
+    }
+}
