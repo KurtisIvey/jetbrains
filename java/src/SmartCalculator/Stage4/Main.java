8e1f0217e953873b13bab9a3b8822ff353ee62d5
KurtisIvey <kurtiveycodes@gmail.com>
2023-11-23 11:39:05 -0600

    implement for loop solution

--- java/src/SmartCalculator/Stage4/Main.java
+++ java/src/SmartCalculator/Stage4/Main.java
@@ -10,11 +10,9 @@ public class Main {
         // take whole line as input to split into array
         while (true) {
             String inputLine = scanner.nextLine();
+            // splits by spaces and also removes whitespace. better than " " since trim not needed
             String[] input = inputLine.split(" ");
-            // removes empty spaces in input arr
-            String[] filteredArray = Arrays.stream(input)
-                    .filter(s -> !s.trim().isEmpty())
-                    .toArray(String[]::new);
+
 
             if (input[0].equals("/exit")) {
                 System.out.println("Bye!");
@@ -24,24 +22,48 @@ public class Main {
             } else if (input[0].isEmpty()) {
                 continue;
             } else {
-                System.out.println(Arrays.toString(filteredArray));
-                //System.out.println(sumStringArray(nums));
-                //continue;
+                //System.out.println(Arrays.toString(input));
+                int result = evaluateExpression(input);
+                System.out.println(result);
+
             }
         }
 
     }
 
-    public static int sumStringArray(String[] stringArray) {
-        int sum = 0;
 
-        for (String str : stringArray) {
-            try {
-                int number = Integer.parseInt(str);
-                sum += number;
-            } catch (NumberFormatException e) {
+    public static int evaluateExpression(String[] expression) {
+        // set initial result to first int in expression arr
+        int result = Integer.parseInt(expression[0]);
+        // handle consecutive minus by changing to + or - alone by mutating express []
+        for (int i = 1; i < expression.length; i += 2) {
+            if(expression[i].contains("-") && expression[i].length() % 2 == 0) {
+                expression[i] = "+";
+            } else if (expression[i].contains("-") && expression[i].length() % 2 == 1) {
+                expression[i] = "-";
+            } else {
+                expression[i] = String.valueOf(expression[i].charAt(0)); // Replace with the desired character or string
             }
         }
-        return sum;
+        //System.out.println(Arrays.toString(expression));
+
+        for (int i = 2; i < expression.length; i += 2) {
+                String operator = expression[i - 1];
+                int number = Integer.parseInt(expression[i]);
+
+            if ("+".equals(operator)) {
+                result += number;
+            } else if ("-".equals(operator)) {
+                result -= number;
+            }
+
+        }
+
+
+        //System.out.println(Arrays.toString(expression));
+        return result;
     }
+
+
 }
+
